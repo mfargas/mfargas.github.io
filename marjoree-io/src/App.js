fa93408ee10e8d428e298b7229ed47b80e13874e
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-02-05 11:12:42 -0800

    Set up state for navbar

--- marjoree-io/src/App.js
+++ marjoree-io/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+import {useState} from 'react';
 
-function App() {
+function App(props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header isMenuOpen={isMenuOpen} />
+        
       </header>
     </div>
   );
-}
+  }
 
 export default App;
